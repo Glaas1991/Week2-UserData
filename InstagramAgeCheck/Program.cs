a44c78e915193655635663bae51dd31c57a3ec1d
Gert Laas <glaas@tktk.ee>
2022-04-21 20:48:12 +0300

    control flow

---
+++ InstagramAgeCheck/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InstagramAgeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm kontrollib kas kasutaja on vanem kui 13 või 13 aastat vana;
+            //kui kasutaja on 13 aastat vana või vanem, siis ta võib instagram'i kasutada;
+            //kui kasutaja on noorem, kui 13 aastat, siis ta on liiga noor;
+
+            Console.WriteLine("Palun, siesta oma vanus:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            if (userAge > 13)
+            {
+                Console.WriteLine("Oled piisavalt vana, et instagram'i kontot luua.");
+            }
+            else if (userAge < 13)
+            {
+                Console.WriteLine("Oled liiga noor, et instagram'i kontot luua.");
+            }
+            else
+            {
+                Console.WriteLine("Oledki 13, nüüd saad endale instagram'i kontot luua.");
+            }
+            Console.WriteLine("Kena päeva!");
+
+
+        }
+    }
+}
---
+++ UserAge/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab kasutaja sünniaasta konsoolis;
+            //"Oled sündinud aastal {yearOfBirth};
+
+            Console.WriteLine("Palun sisesta oma vanus:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2022 - userAge;
+
+            Console.WriteLine($"Oled sündinud aastal {yearOfBirth}.");
+
+
+            
+
+        }
+    }
+}
